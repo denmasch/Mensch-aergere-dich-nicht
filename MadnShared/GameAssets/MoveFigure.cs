12db491dc635cc9075896eec3992cc4a3f7f4d5d
JannikMetz <156902066+JannikMetz@users.noreply.github.com>
2026-02-26 13:12:13 +0100

    started adding moveValidation and MoveFigure.cs

---
+++ MadnShared/GameAssets/MoveFigure.cs
@@ -0,0 +1,22 @@
+﻿namespace MadnShared.GameAssets;
+using MadnShared.Enums; 
+
+public class MoveFigure
+{
+    private MoveValidator _validator;
+
+    public void Move(GameboardState gs, Figure fig, Color activePlayer, int diceRollCount )
+    {
+        _validator = new MoveValidator();
+        
+        bool movementAllowed = _validator.validateMove(GameboardState gs, gs, fig, activePlayer, diceRollCount);
+        if (!movementAllowed)
+        {
+           // TODO: Add Logic for Moving player in GameState with given Gameboard rules
+           
+           //Move Captured Figure back to Home
+        }
+        
+    }
+    
+}
---
+++ MadnShared/GameAssets/MoveValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace MadnShared.GameAssets;
+using MadnShared.Enums;
+public class MoveValidator
+{
+    public bool validateMove(GameboardState state, Figure fig, int diceRollCount )
+    {
+        bool isAllowed = false;
+        
+        //TODO: Test if Movement is allowed
+        
+        /* Allowed Movement:
+        - Figure moves from Tile of Path to empty Tile of
+            - Path
+            - own-color Targets 
+                - if diceRollCount does not exceed Targets
+        - Figure captures enemy Figure
+        - Figures gets out of Home Tiles to Path 
+            - only if Path Tile
+                - is empty
+                - is enemy figures -> gets captured
+        */
+
+        return isAllowed;
+    }
+    
+}
