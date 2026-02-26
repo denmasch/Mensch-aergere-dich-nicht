29825df5c4a3ec6260d75451cfb3b5ad9f301435
JannikMetz <156902066+JannikMetz@users.noreply.github.com>
2026-02-26 15:06:24 +0100

    some progress on Move Validation, but still needs to be implemented if fig goes to target

--- MadnServer/Gamelogic/MoveFigure.cs
+++ MadnServer/Gamelogic/MoveFigure.cs
@@ -1,15 +1,15 @@
-﻿namespace MadnShared.GameAssets;
-using MadnShared.Enums; 
+﻿namespace MadnServer.Gamelogic;
+using MadnShared.Enums;
 
 public class MoveFigure
 {
     private MoveValidator _validator;
 
-    public void Move(GameboardState gs, Figure fig, Color activePlayer, int diceRollCount )
+    public void Move(Gameboard gb, Figure fig, Color activePlayer, int diceRollCount )
     {
         _validator = new MoveValidator();
         
-        bool movementAllowed = _validator.validateMove(GameboardState gs, gs, fig, activePlayer, diceRollCount);
+        bool movementAllowed = _validator.ValidateMove(gb, fig, activePlayer, diceRollCount);
         if (!movementAllowed)
         {
            // TODO: Add Logic for Moving player in GameState with given Gameboard rules
---
+++ MadnServer/Gamelogic/MoveValidator.cs
@@ -0,0 +1,102 @@
+﻿namespace MadnServer.Gamelogic;
+using MadnShared.Enums;
+public class MoveValidator
+{
+    public bool ValidateMove(Gameboard gb, Figure fig, Color activePlayer, int diceRollCount )
+    {
+        bool isAllowed = false;
+        
+        //TODO: Test if Movement is allowed
+        
+        /* Allowed Movement:
+        - If 6 is Rolled allow movement again
+        - Figure moves from Tile of Path to empty Tile of
+            - Path
+            - own-color Targets 
+                - if diceRollCount does not exceed Targets
+        - Figure captures enemy Figure
+        - Figures gets out of Home Tiles to Path by rolling 6
+            - only if Path Tile
+                - is empty
+                - is enemy figures -> gets captured
+        */
+        int pathLength = Gameboard.PathLength;
+        // Get to Figure out of Home to Start by rolling 6 
+        if (fig.IsHome == true)
+        {
+            if (diceRollCount == 6)
+            {
+                for (int i = 0; i > pathLength; i++)
+                {
+                    if (gb.Path[i].Color == activePlayer && gb.Path[i].Type == TileType.Start)
+                    {
+                        return IsTileFree(gb.Path[i], activePlayer);
+                    }
+                }
+            }
+        }
+        else
+        {
+            //Check on which Tile is currently occupied by fig
+            int i = 0;
+            while (gb.Path[i].OccupyingFigure != fig)
+            {
+                i++;
+            }
+
+            
+            int currentTile = i;
+            int newTile = 0;
+
+            
+            // Check for end of array
+            if (currentTile + diceRollCount >= gb.Path.Length - 1) // Hope this -1 correct?
+            {
+                newTile = (currentTile + diceRollCount) - gb.Path.Length;
+                bool endOfArray = true;
+            }
+            else
+            {
+                newTile = (currentTile + diceRollCount);
+                bool endOfArray = false;
+            }
+            
+            // TODO: check if Fig can go to Target or not
+            //Check if Target would be skipped
+            for (i = currentTile; i < newTile; i++)
+            {
+                    
+            }
+
+            isAllowed = IsTileFree(gb.Path[newTile], activePlayer);
+
+        }
+
+        return isAllowed;
+    }
+    
+
+    private bool IsTileFree(Tile tile,  Color activePlayer)
+    {
+        if (tile.IsOccupied == true)
+        {
+            // Is Figure of Same color on Tile?
+            if (tile.OccupyingFigure.Color == activePlayer)
+            {
+                return false;
+            }
+            else
+            {
+                // Figure can capture
+                return true;
+            }
+        }
+        return true;
+
+    }
+
+    private bool IsAllowedToTarget()
+    {
+        return true;
+    }
+}
