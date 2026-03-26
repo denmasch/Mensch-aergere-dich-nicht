5ece05f024b9656d840d33d7aec2099c2ed6b94a
Devin Schnurr <devinschnurr@gmail.com>
2026-03-26 16:24:54 +0100

    implement move logic and give list of valid moves after diceroll

---
+++ MadnShared/GameAssets/Move.cs
@@ -0,0 +1,9 @@
+namespace MadnShared.GameAssets;
+
+public class Move
+{
+    public int FigureIndex { get; set; }
+    
+    // Number of steps to move the figure.
+    public int Steps { get; set; }
+}
