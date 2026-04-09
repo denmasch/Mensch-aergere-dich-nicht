41f930850e2bebb3142baf37f121438e4ad376cf
denmasch <devinschnurr@gmail.com>
2026-04-09 15:33:53 +0200

    Print Gameboard with 11x11 koordinates

--- MadnClient/GameFrontend.cs
+++ MadnClient/GameFrontend.cs
@@ -4,6 +4,8 @@ using MadnShared.Messages.ServerToClient;
 using MadnShared.Logger;
 using MadnShared.Messages.Base;
 using MadnShared.Messages.ClientToServer;
+using MadnShared.Enums;
+using MadnShared.GameAssets;
 
 namespace MadnClient
 {
@@ -28,7 +30,7 @@ namespace MadnClient
             Console.WriteLine("W) Würfeln");
             Console.WriteLine("A/D) Figur auswählen");
             Console.WriteLine("Enter) Figur bewegen");
-
+            
             bool stay = true;
             while (stay)
             {
@@ -61,10 +63,79 @@ namespace MadnClient
             await Task.Delay(300);
         }
         
-        private void DrawGameBoard()
+        private void DrawGameBoard(GameboardDTO board)
         {
             Console.Clear();
+            
+            for (int y = 0; y < 11; y++)
+            {
+                for (int x = 0; x < 11; x++)
+                {
+                    DrawTileAt(x, y, board);
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+
+        private void DrawTileAt(int x, int y, GameboardDTO board)
+        {
+            ConsoleColor bg = ConsoleColor.Black;
+            ConsoleColor fg = ConsoleColor.White;
+            string symbol = "  ";
+
+            // The Homes 
+            if (x <= 1 && y <= 1) { bg = ConsoleColor.Yellow; fg = ConsoleColor.DarkYellow; } // top left
+            else if (x >= 9 && y <= 1) { bg = ConsoleColor.Green; fg = ConsoleColor.DarkGreen; } // top right
+            else if (x <= 1 && y >= 9) { bg = ConsoleColor.Blue; fg = ConsoleColor.DarkBlue; } // bottom left
+            else if (x >= 9 && y >= 9) { bg = ConsoleColor.Red; fg = ConsoleColor.DarkRed; } // bottom right
+            
+            // The Path
+            else if ((x >= 4 && x <= 6) || (y >= 4 && y <= 6)) 
+            { 
+                bg = ConsoleColor.Gray; 
+                fg = ConsoleColor.DarkGray; 
+            }
+            
+            // Get Target Tile
+            if (x == 5 && y == 5) { bg = ConsoleColor.Black; fg = ConsoleColor.White; }
+            else if (x >= 1 && x <= 5 && y == 5) { bg = ConsoleColor.Yellow; fg = ConsoleColor.DarkYellow; }
+            else if (x == 5 && y >= 1 && y <= 5 ) { bg = ConsoleColor.Green; fg = ConsoleColor.DarkGreen; }
+            else if (x >= 5 && x <= 9 && y == 5) { bg = ConsoleColor.Red; fg = ConsoleColor.Red; }
+            else if (x == 5 && y >= 5 && y <= 9) { bg = ConsoleColor.Blue; fg = ConsoleColor.DarkBlue; }
+
+            // TODO: get tile from board and check if occupied
+            // if (tile != null && tile.IsOccupied) 
+            // { 
+            //     symbol = "♙ "; 
+            //     fg = GetFigureColor(tile.OccupyingFigure.Color);
+            // }
+
+            if (bg != ConsoleColor.Black) 
+            {
+                Console.BackgroundColor = bg;
+                Console.ForegroundColor = fg;
+                Console.Write(symbol);
+            }
+            else
+            {
+                Console.ResetColor();
+                Console.Write("  ");
+            }
+            Console.ResetColor();
+            Console.Write(" ");
+        }
+        
+        private ConsoleColor GetFigureColor(Color color)
+        {
+            return color switch
+            {
+                Color.Yellow => ConsoleColor.DarkYellow,
+                Color.Green => ConsoleColor.DarkGreen,
+                Color.Blue => ConsoleColor.DarkBlue,
+                Color.Red => ConsoleColor.DarkRed,
+                _ => ConsoleColor.Black
+            };
         }
     }
 }
-
