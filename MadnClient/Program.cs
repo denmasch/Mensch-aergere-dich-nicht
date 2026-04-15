fa255531ce2bc97c780761048a86c5b2b7d7b569
Devin Schnurr <devinschnurr@gmail.com>
2026-04-15 10:37:35 +0200

    fix: set console encoding to UTF-8 for proper character display

--- MadnClient/Program.cs
+++ MadnClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using MadnShared.GameAssets;
+﻿using System.Text;
+using MadnShared.GameAssets;
 using MadnShared.Logger;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
         Logger.AddWriter(new FileWriter("logs/ClientLog.txt"));
         Logger.LogInfo("Starting Client");
         
